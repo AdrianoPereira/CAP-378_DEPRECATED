0f21b08369d7c82e2123a9defa85414b3a6b6f7a
Adriano P. Almeida <adriano.almeida@inpe.br>
2019-10-03 20:50:00 -0300

    feat: add thread to download with pipeline

--- Download.py
+++ Download.py
@@ -2,14 +2,33 @@ import s3fs
 import numpy as np
 import datetime as dt
 import os
+import threading
 
 
+class Thread(threading.Thread):
+    def __init__(self, file, path):
+#        print('started--->>> ')
+        threading.Thread.__init__(self)
+        self.file = file
+        self.path = path
+        
+        
+    def run(self):
+        print('Downloading %s...'%self.file.split('/')[-1])
+        start = dt.datetime.now()
+        aws = s3fs.S3FileSystem(anon=True)
+        aws.get(self.file, '%s/%s'%(self.path, self.file.split('/')[-1]))
+        print('Downloaded in %.2f seconds - %s'%(\
+                (dt.datetime.now()-start).total_seconds(),
+                self.file.split('/')[-1]))
+        
+        
 def to_julian_day(year, month, day):    
     date = dt.datetime.strptime('%d-%d-%d'%(year,month, day), '%Y-%m-%d')    
     return str(date.timetuple().tm_yday).zfill(3)
 
 
-def downloadABI(**kargs):
+def download_o(**kargs):
     if kargs.get('day'):
         day = kargs.get('day')
     if kargs.get('month'):
@@ -19,28 +38,54 @@ def downloadABI(**kargs):
     if kargs.get('ch'):
         ch = kargs.get('ch')
     
+    aws = s3fs.S3FileSystem(anon=True)    
     julian_day = to_julian_day(year, month, day)
     bucket = 'noaa-goes16/ABI-L2-CMIPF' 
     query = aws.ls('%s/%d/%s'%(bucket, year, julian_day))
-    
     hours = np.array(query)
+    path = 'data/%d/%d/%d'%(year, month, day)
     
-    if not os.path.exists('dados/'+str(YEAR)+'/'+str(MONTH)+'/'+str(DAY)):
-        try:
-            os.makedirs('dados/'+str(YEAR)+'/'+str(MONTH)+'/'+str(DAY))
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                raise
+    if not os.path.exists(path):
+        os.makedirs(path)
+
 
     for hour in hours:
         files = aws.ls(hour)
         for file in files:
             if file.find('M3C'+str(ch)) >= 1:
-                print('Downloading %s...'%file.split('/')[-1])
-                aws.get(file, 'dados/'+str(YEAR)+'/'+str(MONTH)+'/'+str(DAY)+'/'+file.split('/')[-1])      
-#        fs.get(files[0], files[0].split('/')[-1])
+                thread = Thread(file, path)
+                thread.start()
+                
+                
+                
+def download(**kargs):
+    if kargs.get('day'):
+        day = kargs.get('day')
+    if kargs.get('month'):
+        month = kargs.get('month')
+    if kargs.get('year'):
+        year = kargs.get('year')
+    if kargs.get('ch'):
+        ch = kargs.get('ch')
+    
+    aws = s3fs.S3FileSystem(anon=True)    
+    julian_day = to_julian_day(year, month, day)
+    bucket = 'noaa-goes16/ABI-L2-CMIPF' 
+    query = aws.ls('%s/%d/%s'%(bucket, year, julian_day))
+    hours = np.array(query)
+    path = 'data/%d/%d/%d'%(year, month, day)
+    
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-#    print(files)
+
+    for hour in hours:
+        files = aws.ls(hour)
+        for file in files:
+            print(file)
+            if file.find('M3C'+str(ch)) >= 1:
+                print('Downloading %s...'%file.split('/')[-1])
+                aws.get(file, '%s/%s'%(path, file.split('/')[-1]))
 
 
 if __name__ == "__main__":
---
+++ plot_state.py
@@ -0,0 +1,10 @@
+from Download import download_o
+
+
+if __name__ == "__main__":
+    DAY=3
+    MONTH=1
+    YEAR=2019
+    CH=11
+
+    download_o(day=DAY, month=MONTH, year=YEAR, ch=CH)
